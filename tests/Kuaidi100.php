47be3eae55804ef1b4f475f131fdf74b58bd1b7c
Landers <luhaixing@qq.com>
2018-03-20 00:58:18 +0800

    新增测试

---
+++ tests/Kuaidi100.php
@@ -0,0 +1,28 @@
+<?php
+
+use Landers\LaravelAms\Constraints\Tests\BaseTestCase;
+use Landers\AmsApp\Modules\Common\Kuaidi100\Kuaidi100Service;
+
+class Kuaidi100Test extends BaseTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+
+    /**
+     * @test
+     */
+    public function testQueryType()
+    {
+        $no = '472503282097';
+        $result = Kuaidi100Service::make()->query($no);
+        $this->assertArrayHasKey('courier', $result);
+        $this->assertArrayHasKey('path', $result);
+
+        $no = '611021231958194';
+        $result = Kuaidi100Service::make()->query($no);
+        $this->assertFalse($result);
+    }
+}
