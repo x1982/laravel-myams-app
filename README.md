7b4d52834bdedaaa2a3fee7b21a8a8b1df613e5a
Landers <luhaixing@qq.com>
2018-03-19 11:11:54 +0800

    upate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
->
->
+* 模块功能：
+
+    * Geetest (web)
+    * Kuaidi100 (service)
+    * ResourceFetchCommand
--- src/Routes/internet.php
+++ src/Routes/internet.php
@@ -1,3 +1,7 @@
 <?php
 use Landers\LaravelAms\Facades\Route;
 
+//极验
+Route::group(['prefix' => 'geetest'], function () {
+    Route::get('captcha', 'Geetest\GeetestController@captcha');
+});
